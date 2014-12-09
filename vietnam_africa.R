010c44c93bdcf7179ecfcc8ea607d531b4df767f
Anh Le <aql3@duke.edu>
2014-12-09 18:50:50 -0500

    finalize vietnam_africa

--- vietnam_africa.R
+++ vietnam_africa.R
@@ -6,39 +6,57 @@ library(cluster)
 
 startyear <- 1975
 endyear <- 2011
+
+# ---- Download data ----
 wanted_indicator <-c("NY.GDP.MKTP.KD", "NY.GDP.PCAP.KD", "SP.POP.TOTL",
   "NV.AGR.TOTL.ZS","NE.EXP.GNFS.ZS", "NY.GDP.TOTL.RT.ZS")
 data_raw <- WDI(country = "all", indicator = wanted_indicator,
             start = startyear, end = endyear, extra = TRUE)
 
-
-#Subset the data to include Vietnam and Sub-Saharan countries only
+# ---- Subset and rename data ----
 data <- subset(data_raw,
                region=="Sub-Saharan Africa (all income levels)" | country=="Vietnam",
                select = country:NY.GDP.TOTL.RT.ZS)
-names(data) <- c("country", "year", "gdp", "gdp.pc", "pop", "agriculture.%gdp", "export.%gdp", "resource.%gdp")
+names(data) <- c("country", "year", "gdp", "gdp.pc", "pop",
+                 "agriculture.%gdp", "export.%gdp", "resource.%gdp")
 
-# Plot
+# ---- Plotting ----
 
-full_plot <- ggplot(data=data, aes(x=year, y=gdp.pc)) +
-  geom_line(aes(group=country), alpha=0.4)
-vietnam <- geom_line(data=subset(data, country=="Vietnam"), col="red")
+# With ggplot2, we can create individual elements that can be joined later
+# base_plot: time series of all countries in the dataset
+base_plot <- ggplot(data=data, aes(x=year, y=gdp.pc)) +
+  geom_line(aes(group=country), alpha=0.2)
+# vietnam_line: time series of Vietnam
+vietnam_line <- geom_line(data=subset(data, country=="Vietnam"),
+                          col="red", size=1.3)
+# outlier_names: names of the countries with "outlier" gdp.pc (top 20% quantile)
 outlier_names <- geom_text(data=subset(data, gdp.pc > quantile(gdp.pc, probs=0.8, na.rm=T) & year == 2010),
-            aes(label=country))
+                           aes(label=country))
+# median_line: the median line summarizing African countries
+median_line <- stat_summary(data=subset(data, country!="Vietnam"),
+                            fun.data="median_hilow",
+                            geom="smooth", conf.int=0.5)
 
-full_plot + vietnam
-full_plot + vietnam + outlier_names
-full_plot + vietnam + outlier_names + coord_cartesian(ylim=c(0, 2000)) + geom_smooth(method="lm")
-full_plot + vietnam + outlier_names + coord_cartesian(ylim=c(0, 1500)) + geom_smooth(method="lm")
-# Similarity
+# Now we can add these individual elements together as we wish
+base_plot
+base_plot + vietnam_line
+base_plot + vietnam_line + outlier_names
+base_plot + vietnam_line + outlier_names +
+  median_line + coord_cartesian(ylim=c(0, 1500)) +
+  theme_minimal() +
+  labs(title="Vietnam vs Africa",
+       x="Year",
+       y="GDP per capita (2005 USD)")
+
+
+# ---- Gower similarity ----
 
 data_avg <- ddply(data, .(country), colwise(mean, na.rm=TRUE))
 data_sim <- data_avg[ , c("pop", "agriculture.%gdp", "export.%gdp", "resource.%gdp")]
 rownames(data_sim) <- data_avg$country
 
-vietnam_idx <- which(data_sim$country=="Vietnam")
-distance <- as.matrix(daisy(data_sim, metric="gower"))[vietnam_idx, ]
-
+distance_matrix <- as.matrix(daisy(data_sim, metric="gower"))
+distance_vietnam <- distance_matrix[dimnames(distance_matrix)[[1]]=="Vietnam", ]
 
 data_group <- subset(data, country %in% names(distance)[distance < median(distance, na.rm=TRUE)])
 ggplot(data=data_group, aes(x=year, y=gdp.pc)) +
